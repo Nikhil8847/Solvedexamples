4bc0627e06a67bc3c55f57a521e4178492e4c73b
Nikhil8847 <54837275+Nikhil8847@users.noreply.github.com>
2019-12-18 13:43:13 +0530

    solution for vairable sized array

--- Hackerranksolution/variablesizedarray.cpp
+++ Hackerranksolution/variablesizedarray.cpp
@@ -1,1 +1,35 @@
+/*constraints given are/
+1<= n <= 10^5/
+1<=q <= 10^5/
+similarly k/
+i varies from 0 to value of n/
+j varies from 0 to value of k*/
 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n; // n is the constraint given in the problem
+  int q; // similarly a constraint
+  cin>> n>>  q;
+  vector<int> arr[n]; // defining a dynamic array for the storage of our input values
+  for( int i = 0; i< n; i++)
+  {
+    int k; // declaratio of k
+    cin>>k; // constraint for the integer j
+    int a; // taking any integer
+    for(int j = 0; j<k; j++)
+    {
+      cin >> a;
+      arr[i].push_back(a); // i.e. pushing value at a in the array arr[i] 
+    }
+  }
+  int r,s;
+  for(k = 1;  k<= q; k++) // i.e. k is increasing in the constraint q 
+  {
+    cin>> r>> s;
+    cout<< arr[r][s] << endl;  // it is behaving like multidimensional array where r is row and s in column
+  }
+  return 0;
+  
+}
