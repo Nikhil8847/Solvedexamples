0ecf47137e972bb1546be0c76efeb3a111e4c135
Nikhil8847 <54837275+Nikhil8847@users.noreply.github.com>
2019-12-19 02:20:52 +0530

    Update numberofcharacters.cpp

--- stringproblems/numberofcharacters.cpp
+++ stringproblems/numberofcharacters.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
 
+int count(char ch) {
+  string str;
+  getline(cin, str);
+  int count = 0;
+  cin >> ch;
+  for(int i=0; i < str.length(); i++)
+  {
+    if(str[i] == ch){
+    count++;
+  }
+  }
+  return count;
+  /* code */
+}
+int main()
+{
+  extern string str;
+  char c;
+  cout<< count(c);
+  return 0;
+}
