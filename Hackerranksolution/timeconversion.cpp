47c959d0e44c4a668543c3b8ae6726b5b140cd5c
Nikhil8847 <54837275+Nikhil8847@users.noreply.github.com>
2019-12-30 17:23:01 +0530

    Update timeconversion.cpp

--- Hackerranksolution/timeconversion.cpp
+++ Hackerranksolution/timeconversion.cpp
@@ -1,1 +1,11 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int h, m, s;
+	char ch, AorP;
+	cin>> h>> ch>> m>> ch>> s>> AorP >> ch;
+	h = (AorP == 'A') ? (h== 12 ? h : 12) : (h== 12? 0 : h+12);
+	cout<< setw(2) << setfill('0') << h<< ":" << setw(2) << setfill('0') << m<< ":" << setw(2) << setfill('0') << s<< endl;
+	 return 0;
+ }
